fdafd3ef97ad9d02360b85820a2968b0f9583f79
Esteban Rao <estebanrao@gmail.com>
2020-03-16 13:59:22 -0300

    - Broken our homepage into smaller components - Styles menu item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+export const HomePage = () => (
+  <div className="hoempage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
