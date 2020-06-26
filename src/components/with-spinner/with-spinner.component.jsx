77a93eeb208fd96bd84a8f3557751bb627db0439
Esteban Rao <estebanrao@gmail.com>
2020-06-26 14:38:08 -0300

    Adding perf optimizations using lazy, suspense and memo

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component';
 
-const WithSpinner = (WrappedComponent) => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps}></WrappedComponent>
-    );
-  };
-
-  return Spinner;
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <WrappedComponent {...otherProps}></WrappedComponent>
+  );
 };
 
 export default WithSpinner;
